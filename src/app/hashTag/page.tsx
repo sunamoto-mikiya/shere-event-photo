5ca02b4b7a451b7f35d247f25a4e73cb38e7a684
sunamoto-mikiya <miki38gtr@gmail.com>
2024-05-10 23:28:03 +0900

    お気に入り登録

--- src/app/hashTag/page.tsx
+++ src/app/hashTag/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import {
   Table,
@@ -19,29 +18,53 @@ interface HashTag {
   tag: string;
 }
 
-async function fetchHashTags() {
-  const res = await fetch("http://localhost:3000/api/hashTag", {
-    cache: "no-store",
-  });
+const HashTagsPage = () => {
+  const [hashTags, setHashTags] = useState<HashTag[]>([]);
+  const [selectedHashTags, setSelectedHashTags] = useState<{
+    [key: number]: boolean;
+  }>({});
 
-  const data = await res.json();
-  return data.posts;
-}
-const HashTagsPage = async () => {
-  const hashTags = await fetchHashTags();
+  useEffect(() => {
+    const fetchHashTags = async () => {
+      const res = await fetch("http://localhost:3000/api/hashTag", {
+        cache: "no-store",
+      });
+      const data = await res.json();
+      setHashTags(data.posts);
+    };
+    fetchHashTags();
+  }, []);
+
+  const handleFavoriteRegistration = async () => {
+    const key = Object.keys(selectedHashTags);
+
+    // 選択された各IDに対して個別のPOSTリクエストを送信
+    await fetch("http://localhost:3000/api/hashTag/regiFav", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: 1, hashtagId: parseInt(key[0]) }),
+    });
+  };
+
+  const handleCheckboxChange = (id: number) => {
+    setSelectedHashTags((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
 
   return (
     <>
       <div className="container mt-5">
         <div className="d-flex justify-content-between align-items-center mb-3">
           <h1 className="h3">ハッシュタグ一覧</h1>
-          <Button asChild className="m-2">
-            <Link href={"http://localhost:3000/hashTag/create"}>新規作成</Link>
+          <Button onClick={handleFavoriteRegistration} className="m-2">
+            お気に入り登録
           </Button>
           <Button asChild className="m-2">
-            <Link href={"http://localhost:3000/hashTag/regiFav"}>
-              お気に入り登録
-            </Link>
+            <Link href={"http://localhost:3000/hashTag/create"}>新規作成</Link>
           </Button>
         </div>
       </div>
@@ -54,12 +77,15 @@ const HashTagsPage = async () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {hashTags.map((hashTag: HashTag) => (
+          {hashTags.map((hashTag) => (
             <TableRow key={hashTag.id}>
               <TableCell className="font-medium">{hashTag.id}</TableCell>
               <TableCell>{hashTag.tag}</TableCell>
               <TableCell>
-                <Checkbox />
+                <Checkbox
+                  checked={!!selectedHashTags[hashTag.id]}
+                  onCheckedChange={() => handleCheckboxChange(hashTag.id)}
+                />
               </TableCell>
             </TableRow>
           ))}

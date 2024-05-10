bfffa9f9db805f4288697ee51be058d2c1e27e06
sunamoto-mikiya <miki38gtr@gmail.com>
2024-05-10 15:25:56 +0900

    hashTagのapi作成

---
+++ src/app/api/hashTag/route.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+  try {
+    await prisma.$connect();
+  } catch (err) {
+    return Error("DB接続に失敗しました");
+  }
+}
+
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    await main();
+    const posts = await prisma.hashTag.findMany({
+      orderBy: { id: "asc" },
+    });
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const POST = async (req: Request, res: NextResponse) => {
+  console.log("POST");
+
+  try {
+    const { tag, posts, users } = await req.json();
+    await main();
+    const post = await prisma.hashTag.create({
+      data: { tag, posts, users },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
